1c2f4be48f8af56fb67b6c05512dc431baa71454
githubscientist <sathish.datascientist@gmail.com>
2023-11-22 20:45:22 +0530

    readme.js file added with git steps

---
+++ readme.js
@@ -0,0 +1,28 @@
+/*
+    1. Open terminal in mac or linux & command prompt in windows
+    2. dir command - list all files and folders in current directory
+    3. cd command - change directory
+    4. cd .. - go back to parent directory
+    5. cd Desktop or cd Documents - go to Desktop or Documents directory
+    6. mkdir foldername command - create a new directory
+    7. cd foldername - go to foldername directory
+    8. open the folder in vscode - either code . or open visual studio code and open the folder
+    9. check the version of the git installed by 
+        - open the terminal inside vscode from terminal menu
+        - git --version or git -v
+    10. open github.com and create a new repository
+        - give the repository name
+        - leave the visibility as public
+        - create repository
+    11. copy the url of the repository
+    12. configure the git with the username and email (only once)
+        - git config --global user.name "your username"
+        - git config --global user.email "your email"
+    13. add the remote repository url to the local repository
+        - git remote add origin "https://github.com/sathish-fullstackdeveloper/b54wd.git"
+    14. check the remote repository url is added or not
+        - git remote -v
+    15. check the status of the local repository to know the files which are modified
+        - git status
+    16. add the files to the staging area
+*/
