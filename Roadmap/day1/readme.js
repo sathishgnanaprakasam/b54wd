deb7adf30567e11b2bbcf8c811857c465ffdea62
githubscientist <sathish.datascientist@gmail.com>
2023-11-23 21:35:29 +0530

    day 1 completed

--- Roadmap/day1/readme.js
+++ Roadmap/day1/readme.js
@@ -8,4 +8,60 @@
         - npm https://www.npmjs.com/get-npm (not necessary as it comes with NodeJS)
         - Browser (Chrome, Firefox, Edge, Safari)
         - Netlify Account https://www.netlify.com/
+    
+    Introduction to web Browser Wars DOM tree CSSOM tree:
+        - Web browser: A web browser is a software application for accessing information on the World Wide Web.
+            examples: Chrome, Firefox, Edge, Safari, Opera, Brave, etc.
+        
+        History of web browser:
+        
+        - first web browser
+            - WorldWideWeb (later renamed to Nexus to avoid confusion between the software and the World Wide Web)
+            - invented by Tim Berners-Lee in 1990
+            - written in Objective-C programming language
+            - discontinued in 1994
+        - Netscape Navigator
+            - invented by Marc Andreessen in 1994
+            - written in C programming language
+            - discontinued in 2008
+        - Internet Explorer
+            - invented by Thomas Reardon in 1995
+            - written in C++ programming language
+            - discontinued in 2015
+        - War between Netscape Navigator and Internet Explorer
+        - Browser War II between Internet Explorer and Chrome
+        - Browser War III between Chrome and Firefox
+    
+    What is JavaScript?
+        - JavaScript is a programming language that adds interactivity to your website.
+        - Created by Brendan Eich in 1995
+        - Created by Netscape Navigator
+        - Initially named as Mocha, later renamed to LiveScript, and finally renamed to JavaScript
+        - Renamed to Javascript because of the popularity of Java programming language
+        - JScript is the Microsoft's version of JavaScript
+        - JScript Vs JavaScript
+            - JScript is the Microsoft's version of JavaScript
+            - JScript was introduced in 1996
+            - JScript was introduced to compete with JavaScript
+            - JScript is used in Internet Explorer
+            - JScript is not used in other browsers
+            - JScript is not used anymore
+        - JavaScript is a scripting language
+        - It is standardized by ECMA International in the ECMAScript Language Specification
+        - ECMAScript is the standard version of JavaScript
+
+    Versions of JavaScript:
+        - ES1 (ECMAScript 1) - 1997
+        - ES2 (ECMAScript 2) - 1998
+        - ES3 (ECMAScript 3) - 1999
+        - ES4 (ECMAScript 4) - abandoned
+        - ES5 (ECMAScript 5) - 2009
+        - ES6 (ECMAScript 6) - 2015 (major update and popular version)
+        - ES7 (ECMAScript 7) - 2016
+        - ES8 (ECMAScript 8) - 2017
+        - ES9 (ECMAScript 9) - 2018
+        - ES10 (ECMAScript 10) - 2019
+        - ES11 (ECMAScript 11) - 2020
+        - ES12 (ECMAScript 12) - 2021
+
 */
