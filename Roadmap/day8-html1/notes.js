c347e5e25a0c4ede5ee113a751c706292a1ca7e0
githubscientist <sathish.datascientist@gmail.com>
2023-12-08 21:31:43 +0530

    day8

---
+++ Roadmap/day8-html1/notes.js
@@ -0,0 +1,49 @@
+/*
+    What is HTML & Why use it?
+
+    - HTML stands for Hyper Text Markup Language
+    - HTML is the standard markup language for creating Web pages
+    - HTML describes the structure of a Web page
+    - HTML consists of a series of elements
+
+    <!DOCTYPE html> => to tell the browser that this is an HTML5 document
+
+    <html> => the root element of an HTML page
+
+    html tag has two child tags:
+        1. head tag
+            - <head> => the head element is a container for metadata (data about data) and is placed between the <html> tag and the <body> tag
+            - title tag, external file links, internal file links, meta tag, style tag, script tag, social media information meta tags, etc.
+        2. body tag
+            - <body> => the body element contains all the contents of an HTML document, such as headings, paragraphs, images, hyperlinks, tables, lists, etc.
+
+        Basic HTML Tags:
+
+        1. heading tags
+            - <h1> => heading 1
+            - <h2> => heading 2
+            - <h3> => heading 3
+            - <h4> => heading 4
+            - <h5> => heading 5
+            - <h6> => heading 6
+        
+        2. paragraph tag
+            - <p> => paragraph
+        
+        3. bold tag and italic tag
+            - <b> => bold or strong <strong>
+            - <i> => italic or emphasis <em>
+
+            Both the bold and strong tag are used to make the text bold. The only difference is that the strong tag adds semantic meaning to the text. The same goes for the italic and emphasis tag.
+
+            semantic meaning => It adds a meaning to the text which helps the search engine, browser, screen reader, etc. to understand the text better.
+        
+        4. List tags
+            There are two types of lists:
+
+            a. Ordered list
+            b. Unordered list
+
+        All the tags will have attributes. Attributes are used to provide additional information about an element. 
+
+*/
