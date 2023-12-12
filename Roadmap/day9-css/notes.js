2679bf5be2b30e4a6de40e56059701394daefece
githubscientist <sathish.datascientist@gmail.com>
2023-12-12 21:02:37 +0530

    day 9

---
+++ Roadmap/day9-css/notes.js
@@ -0,0 +1,88 @@
+/*
+    CSS: Cascading Style Sheets
+
+    - CSS is a language that describes the style of an HTML document.
+    - CSS describes how HTML elements should be displayed.
+    - CSS is a rule-based language - you define rules specifying groups of styles that should be applied to particular elements or groups of elements on your web page.
+
+    CSS Syntax
+
+    Rule:
+
+    selector {
+        property: value;
+    }
+
+    A CSS file contains multiple rules, each consisting of a selector and a declaration block.
+
+    Different Ways to use CSS:
+        1. Internal CSS
+        2. Inline CSS
+        3. External CSS
+    
+    Selectors:
+        1. element selector
+        2. class selector
+        3. id selector
+--------------------------------
+        4. attribute selector
+        5. pseudo selector
+        6. pseudo element selector
+        7. combinators
+        8. multiple selectors
+    
+    precedence order:
+
+    1. inline styles
+    2. internal styles
+    3. id selectors
+    4. class selectors
+    5. element selectors or tag selectors
+    6. external styles
+    7. universal selectors or global selectors or * selectors
+
+    Inline and Block Elements:
+
+    Inline elements:
+        - do not start on a new line
+        - take only the necessary width
+        - cannot have width and height set
+        - margin and padding push other elements horizontally, but not vertically
+    
+    Block elements:
+        - start on a new line
+        - take the full width available
+        - can have width and height set
+        - margin and padding push other elements both horizontally and vertically
+
+    Inline Elements:
+        - <span>
+        - <a>
+        - <img>
+        - <input>
+        - <button>
+        - <textarea>
+        - <label>
+        - <select>
+        - <strong>
+        - <em>
+        - <i>
+        - <b>
+        - <u>
+        - <sup>
+        - <sub>
+    
+    Block Elements:
+        - <div>
+        - <p>
+        - <h1> to <h6>
+        - <ul>
+        - <ol>
+        - <li>
+        - <table>
+        - <form> 
+    
+    a class can be applied to multiple elements
+    and an element can have multiple classes
+
+*/
---
+++ Roadmap/day9-css/tasks.js
@@ -0,0 +1,38 @@
+/*
+    Task #1
+
+    Create a HTML file and add the following elements to it:
+        - a paragraph with the text "Hello World!"
+        - Using inline CSS, change the color of the text to red.
+    
+    Task #2
+
+    Create a HTML file and add the following elements to it:
+        - a paragraph with the text "Welcome to our website!"
+        - Using internal CSS, set the following styles:
+            - change the color of the text to blue
+            - change the font size to 20px
+            - change the font family to Arial
+            - change the background color to yellow
+    
+    Task #3
+
+    Create a HTML file and add the following elements to it:
+        - a paragraph with the text "Hello World!"
+        - a unordered list with 3 list items:
+            - "item 1"
+            - "item 2"
+            - "item 3"
+        - a button with the text "Click me!"
+
+    Using external CSS, set the following styles:
+        - change the color of the text to red for the paragraph
+        - change the color of the text to blue for the list items
+        - change the color of the text to green for the button
+        - change the font size to 20px for the paragraph
+        - change the font size to 30px for the list items
+        - change the font size to 40px for the button
+        - change the font family to Arial for the paragraph
+        - change the font family to Times New Roman for the list items
+        - change the font family to Courier New for the button
+*/
