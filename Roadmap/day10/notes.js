635f6a2d9e0301f253b4b851f79174914b274733
githubscientist <sathish.datascientist@gmail.com>
2023-12-13 21:38:26 +0530

    day 10

---
+++ Roadmap/day10/notes.js
@@ -0,0 +1,92 @@
+/*
+    Intro to pseudo-classes (link, visited,hover, active)
+
+    pseudo classes are used to define a special state of an element.
+
+
+    for a anchor tag, we have 4 pseudo classes:
+        link: a link that has not been visited
+        visited: a link that has been visited
+        hover: when you move the mouse over an element
+        active: when an element is being activated by a user
+    
+    for a button tag, we have 2 pseudo classes:
+        hover: when you move the mouse over an element
+        active: when an element is being activated by a user
+    
+    for a input text tag, we have 2 pseudo classes:
+        focus: when an element is being focused by a user
+        hover: when you move the mouse over an element
+    
+    for list items, we have 3 pseudo classes:
+        first-child: the first child of a parent element
+        last-child: the last child of a parent element
+        nth-child(n): the nth child of a parent element
+    
+    for a table, we have 3 pseudo classes:
+        first-child: the first child of a parent element
+        last-child: the last child of a parent element
+        nth-child(n): the nth child of a parent element
+        hover: when you move the mouse over an element
+    
+    
+    box model: padding, margin, border
+
+    box-model: is a way of representing the space occupied by an element on a webpage.
+
+    box-model is made up of 4 parts:
+        content: the content of an element
+        padding: the space between the content and the border
+        border: the border of an element
+        margin: the space between the border and the next element
+
+    border: 
+        - solid
+        - dashed
+        - dotted
+        - double
+        - groove
+        - ridge
+    
+    margin:
+        can be set using:
+            margin: 10px; all sides
+            margin-left: 10px; left side
+            margin-right: 10px; right side
+            margin-top: 10px; top side
+            margin-bottom: 10px; bottom side
+
+        shorthand:
+                
+            margin: 10px 20px 30px 40px; // top, right, bottom, left
+
+            margin: 10px 20px 30px; // top, right-left, bottom
+
+            margin: 10px 20px; // top-bottom, right-left
+
+            margin: 10px; // all sides
+        
+    padding: 
+        padding: 10px; // all sides
+        padding-left: 10px; // left side
+        padding-right: 10px; // right side
+        padding-top: 10px; // top side
+        padding-bottom: 10px; // bottom side
+
+        shorthand:
+
+            padding: 10px 20px 30px 40px; // top, right, bottom, left   
+
+            padding: 10px 20px 30px; // top, right-left, bottom
+
+            padding: 10px 20px; // top-bottom, right-left
+
+            padding: 10px; // all sides
+    
+    border:
+        border: 1px solid black; // width, style, color
+        border-left: 1px solid black; // width, style, color
+        border-right: 1px solid black; // width, style, color
+        border-top: 1px solid black; // width, style, color
+        border-bottom: 1px solid black; // width, style, color
+*/
