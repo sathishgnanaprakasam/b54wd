87e7b5bfa4d91d74e70eefea8861e8d52fa1746f
githubscientist <sathish.datascientist@gmail.com>
2023-12-14 21:29:13 +0530

    day 10 additional

---
+++ Roadmap/day10-addnl/flexTask.js
@@ -0,0 +1,14 @@
+/*
+    Exercise: Create a navigation menu using flexbox
+
+    1. Create a navigation menu with 4 links: Home, About, Contact, and Blog
+    2. Use flexbox to display the links horizontally
+    3. Add a hover effect to the links changing the background color when hovered over
+    4. apply the flexbox properties to the links to make them evenly spaced
+    5. Add a border around the links
+    6. Add a border radius to the links
+    7. Add a margin to the links
+    8. Add a padding to the links
+    9. Add a background color to the links
+    10. center the links vertically within the navigation bar
+*/  
---
+++ Roadmap/day10-addnl/notes.js
@@ -0,0 +1,6 @@
+/*
+    Flex in CSS
+        - Flexbox is a layout mode in CSS
+        - It is used to design flexible responsive layout structure without using float or positioning.
+        - Flexbox is a one-dimensional layout method for laying out items in rows or columns.
+*/
