fa108c9fb82868ed8068d965d667cc100670c5ec
githubscientist <sathish.datascientist@gmail.com>
2023-12-18 21:36:07 +0530

    day 12

---
+++ Roadmap/day12/notes.js
@@ -0,0 +1,15 @@
+/*
+    RWD: Responsive Web Design
+
+    Its an approach to web design that aims to make the web pages render well on a variety of devices and different window or screen sizes.
+
+    Responsive web design is becoming more important as the amount of mobile traffic now accounts for more than half of total internet traffic.
+
+    Mobile-First Approach
+
+    Mobile-first approach is exactly what it sounds like: designing for the smallest screen and working your way up. This approach is the opposite of the traditional way of designing for the desktop first and then for mobile.
+
+    Bootstrap
+
+    - Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS- and (optionally) JavaScript-based design templates for typography, forms, buttons, navigation, and other interface components.
+*/
